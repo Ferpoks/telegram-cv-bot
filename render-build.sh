0f7199d53c0c12fae486ead3df255a9dd2e99fb1
Ferpoks <148285162+Ferpoks@users.noreply.github.com>
2025-08-18 12:56:11 +0300

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-set -e
+set -eux
 python -m pip install --upgrade pip
-pip install -r requirements.txt
-# Don't create /var/data at build time; Render build FS is read-only.
+pip install --no-cache-dir -r requirements.txt
